48e6b244464d950d6c7b17f66a825b2dae6df21f
volt-oht <106638531+volt-oht@users.noreply.github.com>
2022-11-17 13:13:34 +0900

    Update logmessaged.py

--- selfdrive/logmessaged.py
+++ selfdrive/logmessaged.py
@@ -17,8 +17,7 @@ def main() -> NoReturn:
   sock.bind("ipc:///tmp/logmessage")
 
   # and we publish them
-  log_message_sock = messaging.pub_sock('logMessage')
-  error_log_message_sock = messaging.pub_sock('errorLogMessage')
+  pub_sock = messaging.pub_sock('logMessage')
 
   while True:
     dat = b''.join(sock.recv_multipart())
@@ -30,12 +29,7 @@ def main() -> NoReturn:
     # then we publish them
     msg = messaging.new_message()
     msg.logMessage = record
-    log_message_sock.send(msg.to_bytes())
-
-    if level >= 40:  # logging.ERROR
-      msg = messaging.new_message()
-      msg.errorLogMessage = record
-      error_log_message_sock.send(msg.to_bytes())
+    pub_sock.send(msg.to_bytes())
 
 
 if __name__ == "__main__":
